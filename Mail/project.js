d979144b68f52ceb7ebed06be7c761487e971e37
019Mahir019 <95768832+019Mahir019@users.noreply.github.com>
2021-12-08 21:00:12 +0400

    Add files via upload

---
+++ Mail/project.js
@@ -0,0 +1,18 @@
+function runBjk() {
+    var a = document.getElementById("bjk");
+    a.innerHTML = "Most of the times, being the third biggest club in your city is not a great source of joy and pride, but Beşiktaş (pronounced “beshiktash”) are one of the few exceptions to this universal unwritten rule. They may have less titles, a smaller stadium and a smaller fan base than their two huge city rivals, Galatasaray and Fenerbahçe, but no matter what, they are a true football megapower in Turkey, having earned respect at international level as well.";
+    var b = document.getElementById("id1");
+    b.innerHTML = "";
+}
+function runFb() {
+    var a = document.getElementById("fb");
+    a.innerHTML = "Based in the Asian side of Istanbul, Fenerbahçe have long ago reached the point of being considered a European great, proof of which is that in 2006, just before celebrating the 100th anniversary of their founding, they were the first Turkish club that got invited to participate in a G-14 (the predecessor of today's European Club Association) conference, an invitation reserved only for top, on and off the field, European clubs.";
+    var b = document.getElementById("id2");
+    b.innerHTML = "";
+}
+function runGs() {
+    var a = document.getElementById("gs");
+    a.innerHTML = "Galatasaray are indeed the historic number one club in Turkey, although Fenerbahçe and, to a lesser extent, Beşiktaş fans may have their objections. Looking at Galatasaray's numbers it's rather safe to claim that they are the biggest Turkish club. Having won more Championship and Cup titles than their Istanbul archrivals, and, very importantly, being the only Turkish club to have won a major UEFA competition.";
+    var b = document.getElementById("id3");
+    b.innerHTML = "";
+}
